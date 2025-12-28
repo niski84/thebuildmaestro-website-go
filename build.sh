46018732b3513b45cf134afa4e35ac54365c5996
Developer <nickskitch@gmail.com>
2025-12-27 23:00:50 -0600

    Add build.sh script for Cloudflare Pages deployment

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Building static site generator..."
+go mod download
+go build -o sitegen ./cmd/sitegen
+
+echo "Generating static site..."
+./sitegen -content static/content -template templates -static static -out public -domain https://thebuildmaestro.com
+
+echo "Build complete! Output in public/ directory"
+

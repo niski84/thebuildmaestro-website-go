7b2f7a4c2d58be470ddf288985145baddf424fed
Developer <nickskitch@gmail.com>
2025-12-27 23:10:46 -0600

    Remove deploy.sh - using Solution 1 (no deploy command needed for Cloudflare Pages)

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Cloudflare Pages automatically deploys the public/ directory
-# This script is a no-op to satisfy any deploy command requirement
-echo "Deployment handled automatically by Cloudflare Pages"
-exit 0
-
